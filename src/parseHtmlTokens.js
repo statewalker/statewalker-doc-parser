8104c93076410c9c1881bf47bee1a4bb478bd7d7
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-12-01 18:37:39 +0100

    chore: update version

--- src/parseHtmlTokens.js
+++ src/parseHtmlTokens.js
@@ -16,15 +16,9 @@ export function parseHtmlToken(str, i = 0) {
     parseHtmlSymbols(str, i);
 }
 
-export default function parseHtmlTokens(str, i = 0) {
-  const token = {
-    type: "Html",
-    content: [],
-    start: i,
-    end: i,
-  };
+export function* iterateHtmlTokens(str, i = 0) {
   let textStart = i;
-  const flushTextContent = () => {
+  const getTextToken = () => {
     let t;
     if (i > textStart) {
       t = {
@@ -34,22 +28,34 @@ export default function parseHtmlTokens(str, i = 0) {
         end: i,
       };
     }
-    token.end = i;
     textStart = i;
     return t;
   };
   for (; i < str.length; i++) {
     const t = parseHtmlToken(str, i);
     if (t) {
-      const textToken = flushTextContent();
-      if (textToken) token.content.push(textToken);
-
-      token.content.push(t);
-      token.end = textStart = i = t.end;
+      const textToken = getTextToken();
+      if (textToken) yield textToken;
+      yield t;
+      textStart = i = t.end;
       i--;
     }
   }
-  const textToken = flushTextContent();
-  if (textToken) token.content.push(textToken);
+  const textToken = getTextToken();
+  if (textToken) yield textToken;
+}
+
+export default function parseHtmlTokens(str, i = 0) {
+  const token = {
+    type: "Html",
+    content: [],
+    start: i,
+    end: i,
+  };
+  for (let t of iterateHtmlTokens(str, i)) {
+    console.log('>', t.type, t.type === 'HtmlTag' ? t.name.name : '');
+    token.content.push(t);
+    token.end = t.end;
+  }
   return token;
 }
