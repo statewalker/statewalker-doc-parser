0655f9c95b7813ae7a21364af7d29be8ba49c9dd
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2022-12-02 17:13:00 +0100

    remove the newKeyValueParser

--- src/parseHtmlAttribute.js
+++ src/parseHtmlAttribute.js
@@ -1,3 +1,30 @@
-import newKeyValueParser from "./newKeyValueParser.js";
+import skipSpaces from "./skipSpaces.js";
+import parseHtmlAttributeValue from "./parseHtmlAttributeValue.js";
+import parseHtmlName from "./parseHtmlName.js";
 
-export default newKeyValueParser()
+export default function parseHtmlAttribute(str, i = 0) {
+  let name = parseHtmlName(str, i);
+  if (!name) {
+    return;
+  }
+  name.type = "HtmlAttributeName";
+  const token = {
+    type: "HtmlAttribute",
+    name,
+    start: i,
+    end: i,
+  };
+  let value;
+  i = skipSpaces(str, name.end);
+  if (str[i] === "=") {
+    i = skipSpaces(str, i + 1);
+    value = parseHtmlAttributeValue(str, i);
+    if (value) {
+      value.type = "HtmlAttributeValue";
+      i = value.end;
+      token.value = value;
+    }
+  }
+  token.end = i;
+  return token;
+}
