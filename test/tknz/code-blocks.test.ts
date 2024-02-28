bf89838c734dd56ba82b36bdc45f94e9fbcc0b88
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2024-02-28 09:01:14 +0100

    chore: remove inused test code

--- test/tknz/code-blocks.test.ts
+++ test/tknz/code-blocks.test.ts
@@ -1,41 +1,7 @@
 import { describe, expect, it } from "../deps.ts";
-import {
-  TToken,
-  TTokenLevel,
-  TTokenizerMethod,
-  TokenizerContext,
-} from "../../src/tknz/tokenizer.ts";
-import { testData } from "./code-blocks.data.ts";
+import { TToken, TokenizerContext } from "../../src/tknz/tokenizer.ts";
 import { newNgramsWithCode } from "../../src/tknz/blocks.ts";
-
-function newBlockReader(nextToken: TTokenizerMethod): TTokenizerMethod {
-  return (ctx: TokenizerContext): TToken | undefined => {
-    const currentLevel = TTokenLevel.block;
-    let list: TToken[] = [];
-    let token: TToken | undefined;
-    while ((token = nextToken(ctx))) {
-      if (token.level >= currentLevel) {
-        break;
-      }
-      if (token.type === "Eol" && token.value.length > 1) {
-        ctx.i = token.start;
-        break;
-      }
-      list.push(token);
-    }
-    if (!list.length) return;
-    const start = list[0].start;
-    const end = list[list.length - 1].end;
-    return {
-      level: currentLevel,
-      type: "Block",
-      start,
-      end,
-      value: ctx.substring(start, end),
-      children: list,
-    };
-  };
-}
+import { testData } from "./code-blocks.data.ts";
 
 function test(str: string, control: TToken) {
   const ctx = new TokenizerContext(str);
